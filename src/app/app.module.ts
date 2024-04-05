d586f406abe2c544a27e8a983e1d7df8c705d329
Erik Trejo <erik.trejob@gmail.com>
2024-04-05 11:46:26 -0600

    Fin de sección 4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { CounterModule } from './counter/counter.module';
+import { HeroesMolule } from './heroes/heroes.module';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CounterModule,
+    HeroesMolule
   ],
   providers: [],
   bootstrap: [AppComponent]
